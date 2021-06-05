4af44cdeac9aefd735767c25772cea9567c0e7cb
adijoshi82812 <adijoshi82812@gmail.com>
2021-06-05 16:13:44 +0530

    Updated the database model
    Updated to display the unicode instance of a particular class, also removed the secret key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'Write Your secret Key Here'
+SECRET_KEY = 'Write_Your_secret_Key_Here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
