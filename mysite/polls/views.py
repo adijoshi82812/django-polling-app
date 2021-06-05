7289479dcdbea1e3becd4ad268e59d5e6517ee8b
adijoshi82812 <adijoshi82812@gmail.com>
2021-06-05 16:24:28 +0530

    Added more views for results, votes and details
    Added views and url paths for our the views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 def index(response):
     return HttpResponse("Hello World")
 
+def detail(request, question_id):
+    return HttpResponse("You are viewing question %s." % question_id)
+
+def results(request, question_id):
+    response = "You are viewing results for question %s."
+    return HttpResponse(response % question_id)
+
+def votes(request, question_id):
+    return HttpResponse("You are voting for question %s." % question_id)
+
 # Create your views here.
